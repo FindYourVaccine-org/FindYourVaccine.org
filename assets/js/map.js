59f4524c14a6059942ca0f7759aaeb6f57c87215
Fletcher Heisler <>
2021-01-31 23:25:57 -0500

    Map legend toggle WIP

--- assets/js/map.js
+++ assets/js/map.js
@@ -165,32 +165,24 @@ function makeMap(csvData) {
       var bbox = turf.bbox(data);
       map.fitBounds(bbox, { padding: 50 });
 
-      // add status filters
-      var typesObj = {}, types = [];
-      for (var i = 0; i < data.features.length; i++) {
-        typesObj[data.features[i].properties['Status']] = true;
-      }
-      for (var k in typesObj) { types.push(k) };
-      types.sort();
-
       var checkboxes = [];
       // Create a filter interface.
-      for (var i=0; i < types.length; i++) {
-        // Create an an input checkbox and label inside.
-        var item = filters.appendChild(document.createElement('div'));
-        var checkbox = item.appendChild(document.createElement('input'));
-        var label = item.appendChild(document.createElement('label'));
-        label.className = "filter-label";
-        checkbox.type = 'checkbox';
-        checkbox.id = types[i];
-        checkbox.checked = true;
-        // create a label to the right of the checkbox with explanatory text
-        label.innerHTML = types[i];
-        label.setAttribute('for', types[i]);
-        // Whenever a person clicks on this checkbox, call the update().
-        checkbox.addEventListener('change', update);
-        checkboxes.push(checkbox);
-      }
+      // for (var i=0; i < types.length; i++) {
+      //   // Create an an input checkbox and label inside.
+      //   var item = filters.appendChild(document.createElement('div'));
+      //   var checkbox = item.appendChild(document.createElement('input'));
+      //   var label = item.appendChild(document.createElement('label'));
+      //   label.className = "filter-label";
+      //   checkbox.type = 'checkbox';
+      //   checkbox.id = types[i];
+      //   checkbox.checked = true;
+      //   // create a label to the right of the checkbox with explanatory text
+      //   label.innerHTML = types[i];
+      //   label.setAttribute('for', types[i]);
+      //   // Whenever a person clicks on this checkbox, call the update().
+      //   checkbox.addEventListener('change', update);
+      //   checkboxes.push(checkbox);
+      // }
 
       // Call when someone clicks on a checkbox and changes the selection of markers to be displayed
       function update() {
